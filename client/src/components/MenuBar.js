8aa9d978fdbe854626f514c824a9910f9de01c10
Chuong Truong <chuongtruong290893@gmail.com>
2021-07-11 16:29:58 -0700

    as of 7/11

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
 import { Menu, Segment } from "semantic-ui-react";
+import {Container} from "semantic-ui-react";
+import {Link} from 'react-router-dom';
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState("");
+  
+  // window.location.pathname will only return the path name, not the whole url
+  // for example: http://localhost:3000/ -> pathname is "/"
+  // for example: http://localhost:3000/login -> pathname is "/login"
+  
+  const pathName = window.location.pathname;
+  // console.log(pathName);
+  const path = pathName === '/' ? 'home' : pathName.substr(1)
 
-  const handleItemClick = (e, { name }) => setactiveItem(name);
+  
+  const [activeItem, setActiveItem] = useState(path);
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
+        //this link is ACTIVE when const activeItem === "home"
+        //activeItem is controlled by useState()
         active={activeItem === "home"}
         onClick={handleItemClick}
+        as={Link}
+        to="/"
       />
 
       <Menu.Menu position="right">
@@ -19,11 +34,15 @@ function MenuBar() {
           name="login"
           active={activeItem === "login"}
           onClick={handleItemClick}
+          as={Link}
+          to="/login"
         />
         <Menu.Item
           name="register"
           active={activeItem === "register"}
           onClick={handleItemClick}
+          as={Link}
+          to="/register"
         />
       </Menu.Menu>
     </Menu>
