0b3204aedb20eb5b632cda311080c9d4f0fb079a
Chuong Truong <chuongtruong290893@gmail.com>
2021-07-11 23:48:28 -0700

    as of 7/11

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,5 +1,6 @@
 import Reactfrom from "react";
-import { Card, Icon, Label, Image } from "semantic-ui-react";
+import {Link} from "react-router-dom";
+import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
 import moment from "moment";
 
 //when post is passed from Home, we can destrcucture post like post:{something, something, something}
@@ -17,11 +18,28 @@ function PostCard({
           src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
         />
         <Card.Header>{username}</Card.Header>
-        <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+        <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <p>Some buttons here</p>
+      <Button as='div' labelPosition='right'>
+      <Button color='red'>
+        <Icon name='heart' />
+        Like
+      </Button>
+      <Label as='a' basic color='red' pointing='left'>
+      {likeCount}
+      </Label>
+    </Button>
+    <Button as='div' labelPosition='right'>
+      <Button basic color='blue'>
+        <Icon name='fork' />
+        Fork
+      </Button>
+      <Label as='a' basic color='blue' pointing='left'>
+      {commentCount}
+      </Label>
+    </Button>
       </Card.Content>
     </Card>
   );
