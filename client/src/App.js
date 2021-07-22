f96347937a33206dbc459417c86a4ec3a28bffa9
Chuong Truong <chuongtruong290893@gmail.com>
2021-07-22 01:07:40 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {Container} from 'semantic-ui-react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import MenuBar from './components/MenuBar';
+//need to destructure AuthContext from auth.js
+import { AuthProvider } from "./context/auth";
+
+// AuthRoute doesn't need to be destructured since it's the only fuction that was exported default from AuthRoute.js
+import AuthRoute from './util/AuthRoute'
+
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    //Container is from semantic UI
-    <Container> 
-      {/* later <Container> will have to change to div className="ui container", because of semantic UI*/}
-      <Router>
-        <MenuBar/>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/register" component={Register}/>
-      </Router>
-    </Container>
+    <AuthProvider>
+      {/* Container is from semantic UI */}
+      <Container>
+        {/* later <Container> will have to change to div className="ui container", because of semantic UI*/}
+        <Router>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} /> */}
 
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+        </Router>
+      </Container>
+    </AuthProvider>
   );
 }
 
