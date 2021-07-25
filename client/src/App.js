6d344b46812cbf76e09976735858e7b002515eae
Chuong Truong <chuongtruong290893@gmail.com>
2021-07-25 00:04:20 -0700

    upDATE

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MenuBar from "./components/MenuBar";
+// import SinglePost from "./pages/SinglePost";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
 
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
+          {/* <Route exact path="/post/:postId" component={SinglePost}/> */}
         </Router>
       </Container>
     </AuthProvider>
