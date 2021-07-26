2fcdf97bd53614e9c7ce13c99cb82ee1a82f3114
Chuong Truong <chuongtruong290893@gmail.com>
2021-07-25 22:26:05 -0700

    update

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -4,14 +4,32 @@ import { useMutation } from '@apollo/react-hooks'
 
 import { Button, Confirm, Icon } from 'semantic-ui-react';
 
-function DeleteButton({postId}){
+import {FETCH_POSTS_QUERY} from '../util/graphql';
+
+function DeleteButton({postId, callback}){
     const [confirmOpen, setConfirmOpen] = useState(false);
 
     const [deletePost] = useMutation(DELETE_POST_MUTATION,{
-        update(){
+        //need proxy to modify catch cache
+        update(proxy){
             //close the confirm modal
             setConfirmOpen(false);
-            // TODO: remove post from cache
+
+            // remove post from cache
+            //read the cache
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY,
+            })
+            //filter out deleted result
+            // data.getPosts = data.getPosts.filter(p=> p.id !== postId)
+            //update cache
+            proxy.writeQuery({
+                query: FETCH_POSTS_QUERY,
+                data:{getPosts: data.getPosts.filter(p=> p.id !== postId)}
+            });
+
+            if(callback){callback();} //help redirect to homepage if user hit delete the post 
+
         },
         variables: { postId}
     })
